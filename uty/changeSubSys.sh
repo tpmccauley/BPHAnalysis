bb6bd7cceb5872819db1b9c961f36e2f138878f0
Paolo <paolo.ronchese@cern.ch>
2018-07-16 15:47:49 +0200

    changeSubSys script updated

--- uty/changeSubSys.sh
+++ uty/changeSubSys.sh
@@ -15,7 +15,7 @@ fi
 cd ..
 
 export DLIST=`ls -1d ${DSRC}/* | grep -v uty | awk '{for(i=1;i<NF;++i)printf($i" "); print $NF}'`
-find ${DLIST} | egrep '(h|cc|xml|py)$' | grep -v __init__ | awk -F/ -v DSRC=${DSRC} -v DEST=${DEST} '(NF>1){p=""; for(i=2;i<NF;++i)p=p"/"$i; s=DSRC""p; d=DEST""p; print "mkdir -p "d" ; sed s/"DSRC"/"DEST"/ "s"/"$NF" > "d"/"$NF" ;"}'
-eval `find ${DLIST} | egrep '(h|cc|xml|py)$' | grep -v __init__ | awk -F/ -v DSRC=${DSRC} -v DEST=${DEST} '(NF>1){p=""; for(i=2;i<NF;++i)p=p"/"$i; s=DSRC""p; d=DEST""p; print "mkdir -p "d" ; sed s/"DSRC"/"DEST"/ "s"/"$NF" > "d"/"$NF" ;"}'`
+find ${DLIST} | egrep '(h|cc|C|xml|py)$' | grep -v __init__ | awk -F/ -v DSRC=${DSRC} -v DEST=${DEST} '(NF>1){p=""; for(i=2;i<NF;++i)p=p"/"$i; s=DSRC""p; d=DEST""p; print "mkdir -p "d" ; sed s/"DSRC"/"DEST"/ "s"/"$NF" > "d"/"$NF" ;"}'
+eval `find ${DLIST} | egrep '(h|cc|C|xml|py)$' | grep -v __init__ | awk -F/ -v DSRC=${DSRC} -v DEST=${DEST} '(NF>1){p=""; for(i=2;i<NF;++i)p=p"/"$i; s=DSRC""p; d=DEST""p; print "mkdir -p "d" ; sed s/"DSRC"/"DEST"/ "s"/"$NF" > "d"/"$NF" ;"}'`
 
 cp -r --preserve=all ${DSRC}/uty ${DEST}/uty
